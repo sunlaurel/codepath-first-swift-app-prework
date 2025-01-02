3296e5b56792be95ee22b561dd9b51a7da5e4fc0
Laurel Sun <86388357+sunlaurel@users.noreply.github.com>
2025-01-01 16:54:12 -0800

    added firstapp contents

---
+++ FirstAppDir/FirstAppTests/FirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppTests.swift
+//  FirstAppTests
+//
+//  Created by Laurel Sun on 12/31/24.
+//
+
+import Testing
+@testable import FirstApp
+
+struct FirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
